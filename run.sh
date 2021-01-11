613b02a792e1efda40190a0828486df9f17e88e0
wuzhicong <wuzhicong@aiyunxiao.com>
2021-01-11 22:54:57 +0800

    feat: 引入JSON包，返回JSON格式数据

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 javac -cp \
 ./WEB-INF/lib/servlet-api.jar:\
-./WEB-INF/lib/mysql-connector-java-8.0.21.jar:.\
+./WEB-INF/lib/json-20201115.jar:.\
  Users.java \
 && mv *.class ./WEB-INF/classes/  \
 && ../../bin/shutdown.sh \

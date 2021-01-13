262e9ded58f62a28f1d210c889f7c7996d76a46c
wuzhicong <wuzhicong@aiyunxiao.com>
2021-01-13 22:31:10 +0800

    feat: 通过手动移动class文件和手动添加jar的方式完成项目构建

---
+++ src/main/webapp/servlets/Users.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+public class Users extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("helloworld");
+    }
+}

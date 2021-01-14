e7427e2e30b79af40872cd48adda9eed9f344c65
wuzhicong <wuzhicong@aiyunxiao.com>
2021-01-14 23:24:04 +0800

    feat: org.json返回JSON格式数据

--- src/main/java/servlets/Users.java
+++ src/main/java/servlets/Users.java
@@ -4,11 +4,19 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import org.json.JSONObject;
+
+
 public class Users extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json;charset=UTF-8");
+        JSONObject output = new JSONObject();
+        JSONObject data = new JSONObject();
+        output.put("code", 0);
+        output.put("msg", "获取成功");
+        output.put("data", data);
         PrintWriter out = response.getWriter();
-        out.println("helloworld");
+        out.println(output);
     }
 }

bb1bdf8f8543adf3ef7500843f1511ee92d85851
itgo <itgo@itgodeMacBook-Air.local>
2021-01-21 18:46:31 +0800

    feat: 文件上传和文件获取

--- src/main/java/servlets/Images.java
+++ src/main/java/servlets/Images.java
@@ -3,15 +3,49 @@ package servlets;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-
+import java.util.List;
+import utils.Utils;
 import org.json.JSONObject;
-
+import org.json.JSONException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.FileItem;
 
 public class Images extends HttpServlet {
     /**
      * POST /v1/images
      */
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            // Create a factory for disk-based file items
+            DiskFileItemFactory factory = new DiskFileItemFactory();
+
+            // Configure a repository (to ensure a secure temp location is used)
+            // ServletContext servletContext = this.getServletConfig().getServletContext();
+            // File repository = (File) servletContext.getAttribute("javax.servlet.context.tempdir");
+            // factory.setRepository(repository);
+            // factory.setRepository();
+
+            // Create a new file uploa`d handler
+            ServletFileUpload upload = new ServletFileUpload(factory);
+
+            // Parse the request
+            List<FileItem> items = upload.parseRequest(request);
+            for(FileItem item : items){
+                if(!item.isFormField()){
+                    String name = new File(item.getName()).getName();
+                    item.write( new File("/Users/itgo/Documents/java/apache-tomcat-9.0.41/webapps/sbs-impl/src/main/resources" + File.separator + name));
+                }
+            }
+        } catch (FileUploadException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+
+
         response.setContentType("application/json;charset=UTF-8");
         JSONObject output = new JSONObject();
         JSONObject data = new JSONObject();
@@ -22,4 +56,18 @@ public class Images extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println(output);
     }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("image/png");
+        InputStream fielStream = new FileInputStream(new File("/Users/itgo/Documents/java/apache-tomcat-9.0.41/webapps/sbs-impl/src/main/resources/test.png"));
+        int size = fielStream.available(); //得到文件大小   
+        byte data[] = new byte[size];   
+        fielStream.read(data);  //读数据   
+        fielStream.close();
+        OutputStream os = response.getOutputStream();  
+        os.write(data);  
+        os.flush();  
+        os.close();          
+    }
+    
 }
